a32caeaf9fd4bb00b35397f3f232025c46cc41be
David Barri <japgolly@gmail.com>
2016-11-12 21:11:54 +1100

    +NonEmpty

---
+++ nonEmptiness/shared/src/test/scala/japgolly/pond/nonemptiness/NonEmptyTest.scala
@@ -0,0 +1,19 @@
+package japgolly.pond.nonemptiness
+
+import utest._
+
+object NonEmptyTest extends TestSuite {
+
+  override def tests = TestSuite {
+    'vector - {
+      'empty - assert(NonEmpty(Vector()).isEmpty)
+      'one - assert(NonEmpty(Vector(1)) == Some(NonEmptyVector(1)))
+      'two - assert(NonEmpty(Vector(1, 2)) == Some(NonEmptyVector(1, 2)))
+    }
+    'set - {
+      'empty - assert(NonEmpty(Set.empty[Int]).isEmpty)
+      'one - assert(NonEmpty(Set(1)) == Some(NonEmptySet(1)))
+      'two - assert(NonEmpty(Set(1, 2)) == Some(NonEmptySet(1, 2)))
+    }
+  }
+}
