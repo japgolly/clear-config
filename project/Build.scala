c2c0db7a61049df40cafa30100ee1a1b6ac745e5
David Barri <japgolly@gmail.com>
2016-12-11 17:10:42 +1100

    Update deps

--- project/Build.scala
+++ project/Build.scala
@@ -21,8 +21,8 @@ object Microlibs {
     val Monocle         = "1.3.2"
     val MTest           = "0.4.4"
     val Scala211        = "2.11.8"
-    val Scala212        = "2.12.0"
-    val Scalaz          = "7.2.7"
+    val Scala212        = "2.12.1"
+    val Scalaz          = "7.2.8"
     val UnivEq          = "1.0.2"
 
   }
