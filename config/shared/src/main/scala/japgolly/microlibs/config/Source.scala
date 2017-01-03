961f7bb91e4643ea5248dcbc9e4c1102c0eeb293
David Barri <japgolly@gmail.com>
2017-01-03 14:39:39 +1100

    config: fix source composition

--- config/shared/src/main/scala/japgolly/microlibs/config/Source.scala
+++ config/shared/src/main/scala/japgolly/microlibs/config/Source.scala
@@ -4,14 +4,24 @@ import java.util.Properties
 import scalaz.{Applicative, -\/, \/, \/-}
 
 final case class SourceName(value: String) extends AnyVal
+object SourceName {
+  def environment = SourceName("Environment")
+  def system = SourceName("System")
+}
 
 final case class Source[F[_]](name: SourceName, prepare: F[String \/ ConfigStore[F]]) {
   override def toString: String = s"Source(${name.value})"
+
+  def toSources: Sources[F] =
+    Sources(Vector.empty :+ this)
 }
 
 object Source {
   implicit def toSources[F[_]](s: Source[F]): Sources[F] =
-    Sources(Vector.empty :+ s)
+    s.toSources
+
+  implicit def seqToSources[F[_]](s: Seq[Source[F]]): Seq[Sources[F]] =
+    s.map(_.toSources)
 
   def point[F[_]](name: String, config: => ConfigStore[F])(implicit F: Applicative[F]): Source[F] =
     Source[F](SourceName(name), F.point(\/-(config)))
@@ -19,18 +29,11 @@ object Source {
   def manual[F[_]](name: String)(kvs: (String, String)*)(implicit F: Applicative[F]): Source[F] =
     point(name, ConfigStore.stringMap(kvs.toMap))
 
-  def environmentName = SourceName("Environment")
-
   def environment[F[_]](implicit F: Applicative[F]): Source[F] =
-    point(environmentName.value, ConfigStore.stringMap(sys.env))
-
-  def systemName = SourceName("System")
-
-  // TODO move names to SourceName.xxx
-  def applicativePointName = SourceName("Manual")
+    point(SourceName.environment.value, ConfigStore.stringMap(sys.env))
 
   def system[F[_]](implicit F: Applicative[F]): Source[F] =
-    Source[F](systemName, F.point {
+    Source[F](SourceName.system, F.point {
       def cfg() = ConfigStore.javaProps[F](System.getProperties())
       \/.fromTryCatchNonFatal(cfg()).leftMap(_.getMessage)
     })
@@ -57,16 +60,26 @@ object Source {
 }
 
 final case class Sources[F[_]](highToLowPri: Vector[Source[F]]) extends AnyVal {
-  override def toString: String = s"Sources(${highToLowPri.map(_.name.value) mkString " > "})"
+  override def toString: String =
+    s"Sources(${highToLowPri.map(_.name.value) mkString " > "})"
 
   def >(lowerPri: Sources[F]): Sources[F] =
     Sources(highToLowPri ++ lowerPri.highToLowPri)
 
   def <(higherPri: Sources[F]): Sources[F] =
     higherPri > this
+
+  def reverse: Sources[F] =
+    Sources(highToLowPri.reverse)
 }
 
 object Sources {
   def empty[F[_]]: Sources[F] =
     apply(Vector.empty)
+
+  def highToLowPri[F[_]](ss: Sources[F]*): Sources[F] =
+    apply(ss.iterator.flatMap(_.highToLowPri).toVector)
+
+  def lowToHighPri[F[_]](ss: Sources[F]*): Sources[F] =
+    highToLowPri(ss.reverse: _*)
 }
---
+++ config/shared/src/test/scala/japgolly/microlibs/config/SourceTest.scala
@@ -0,0 +1,30 @@
+package japgolly.microlibs.config
+
+import scalaz.Scalaz.Id
+import utest._
+
+object SourceTest extends TestSuite {
+
+  def s1: Source[Id] = Source.environment
+  def ss: Sources[Id] = s1
+
+  def ensureCompilation(s: Sources[Id]) = ()
+
+  override def tests = TestSuite {
+
+    "1>n" - ensureCompilation(s1 > ss)
+    "1<n" - ensureCompilation(s1 < ss)
+    "n>1" - ensureCompilation(ss > s1)
+    "n<1" - ensureCompilation(ss < s1)
+    "1>1" - ensureCompilation(s1 > s1)
+    "1<1" - ensureCompilation(s1 < s1)
+    "n>n" - ensureCompilation(ss > ss)
+    "n<n" - ensureCompilation(ss < ss)
+
+    * - ensureCompilation {
+      val files = List("a", "b", "c")
+      val sources = files.map(Source.propFileOnClasspath[Id](_, optional = true))
+      Source.environment[Id] > Sources.highToLowPri(sources: _*) > Source.system[Id]
+    }
+  }
+}
