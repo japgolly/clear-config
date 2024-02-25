33ef486ba60e7d66aaf535b046cd8c62aa1c5a31
Scala Steward <scala_steward@virtuslab.com>
2024-02-25 18:36:04 +0000

    Update kind-projector to 0.13.3

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -14,7 +14,7 @@ object Dependencies {
 
     // Internal
     val betterMonadicFor      = "0.3.1"
-    val kindProjector         = "0.13.2"
+    val kindProjector         = "0.13.3"
     val scalaJsJavaTime       = "1.0.0"
     val utest                 = "0.8.1"
   }
